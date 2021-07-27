cfd1a70f4113a808dc7f60f6a015606669ecf723
Brady <bradylill@gmail.com>
2021-07-26 18:35:37 -0600

    Latest golang

--- golang/install.sh
+++ golang/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ ! -x "$(which go)" ]]; then
+if [[ ! -x "$(command -v go)" ]]; then
   if [[ $OSTYPE == "darwin"* ]]; then
     brew install go
-  elif [[ -x "$(which apt)" ]]; then
-    version="1.12.7"
-    checksum="66d83bfb5a9ede000e33c6579a91a29e6b101829ad41fffb5c5bb6c900e109d9"
+  elif [[ -x "$(command -v apt)" ]]; then
+    version="1.16.3"
+    checksum="951a3c7c6ce4e56ad883f97d9db74d3d6d80d5fec77455c6ada6c1f7ac4776d2"
 
-    pushd /tmp
+    pushd /tmp || exit 1
     if [[ ! -r "go$version.linux-amd64.tar.gz" ]]; then
      curl -O "https://dl.google.com/go/go$version.linux-amd64.tar.gz"
     fi
@@ -21,7 +21,7 @@ if [[ ! -x "$(which go)" ]]; then
     sudo tar -C /usr/local/opt -xzf "go$version.linux-amd64.tar.gz"
     sudo ln -s /usr/local/opt/go /usr/local/opt/go/libexec
     sudo ln -s /usr/local/opt/go/bin/go /usr/local/bin/go
-    popd
+    popd || exit 1
 
     mkdir -p "$HOME/go"
   else
