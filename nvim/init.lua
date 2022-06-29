0f07cf7e25fb51e0edc0f3a087f4926a123ad126
Brady <bradylill@gmail.com>
2022-06-29 13:44:24 -0600

    Random updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,24 @@
 -- Plugs
+local Plug = vim.fn['plug#']
+
 vim.fn['plug#begin'](vim.fn.stdpath('data') .. '/plugged')
-vim.fn['plug#']('neovim/nvim-lspconfig', { commit = '99596a8' })
+Plug('neovim/nvim-lspconfig', { commit = '99596a8' })
 
-vim.fn['plug#']('christoomey/vim-tmux-navigator', { commit = '9ca5bfe' })
+Plug('christoomey/vim-tmux-navigator', { commit = '9ca5bfe' })
 
-vim.fn['plug#']('nvim-lua/plenary.nvim', { commit = '1da13ad' })
-vim.fn['plug#']('nvim-telescope/telescope.nvim', { commit = 'cd9e6aa' })
-vim.fn['plug#']('nvim-treesitter/nvim-treesitter', { commit = '26abd5f' })
-vim.fn['plug#']('nvim-lualine/lualine.nvim', { commit = 'c12b167' })
+Plug('nvim-lua/plenary.nvim', { commit = '1da13ad' })
+Plug('nvim-telescope/telescope.nvim', { commit = 'cd9e6aa' })
+Plug('nvim-treesitter/nvim-treesitter', { commit = '26abd5f' })
+Plug('nvim-lualine/lualine.nvim', { commit = 'c12b167' })
 
-vim.fn['plug#']('hrsh7th/nvim-cmp', { commit = '033a817' })
-vim.fn['plug#']('hrsh7th/cmp-nvim-lsp', { commit = 'affe808' })
+Plug('hrsh7th/nvim-cmp', { commit = '033a817' })
+Plug('hrsh7th/cmp-nvim-lsp', { commit = 'affe808' })
 
-vim.fn['plug#']('sheerun/vim-polyglot', { tag = 'v4.17.1' })
+Plug('sheerun/vim-polyglot', { tag = 'v4.17.1' })
 
-vim.fn['plug#']('junegunn/seoul256.vim', { commit = '8f3dd7d' })
+Plug('junegunn/seoul256.vim', { commit = '8f3dd7d' })
+
+Plug('eraserhd/parinfer-rust', { tag = 'v0.4.3', ['do'] = 'cargo build --release'})
 vim.fn['plug#end']()
 
 -- Common nvim config
@@ -45,15 +49,53 @@ vim.opt.encoding = 'utf-8'
 vim.opt.number = true
 vim.opt.swapfile = false
 
+vim.o.updatetime = 250
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
 -- Helper functions
 function map(mode, shortcut, command)
   vim.keymap.set(mode, shortcut, command)
+end
+
+function toggleDarkMode()
+  local current = vim.opt.background._value;
+
+  if (current == 'dark') then
+    vim.opt.background = 'light'
+    vim.g.seoul256_background = 254
+    vim.cmd([[ colorscheme seoul256-light ]])
+  else
+    vim.opt.background = 'dark'
+    vim.g.seoul256_background = 234
+    vim.cmd([[ colorscheme seoul256 ]])
+  end
 end
 
 -- Keymappings
 map('n', '<leader>fed', ':e $MYVIMRC<cr>')
 map('n', '<leader>fer', ':source $MYVIMRC<cr>')
 map('n', '<leader>sc', ':noh<cr>')
+map('n', '<leader>tc', ':lua toggleDarkMode()<cr>')
+
+-- LSP
+map('n', '<F2>', ':lua vim.lsp.buf.rename()<cr>')
+map('n', '<leader>fr', ':lua vim.lsp.buf.references()<cr>')
+map('n', '<leader>fi', ':lua vim.lsp.buf.implementation()<cr>')
+map('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<cr>')
+map('n', '<leader>gd', ':lua vim.lsp.buf.definition()<cr>')
+map('n', '<leader>gt', ':lua vim.lsp.buf.type_definition()<cr>')
+map('n', '<leader>dh', ':lua vim.lsp.buf.hover()<cr>')
+
+-- vim.api.nvim_create_autocmd('CursorHold', {
+--     command = 'lua vim.lsp.buf.document_highlight()'
+--   })
 
 -- nvim-cmp
 local cmp = require'cmp'
@@ -79,10 +121,28 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 -- Typescript
 require'lspconfig'.tsserver.setup {}
 
+-- eslint/json/html/css
+require'lspconfig'.eslint.setup {}
+require'lspconfig'.html.setup {}
+require'lspconfig'.cssls.setup {}
+require'lspconfig'.jsonls.setup {}
+
 -- Terraform
 require'lspconfig'.terraformls.setup {}
 
+-- Clojure
+require'lspconfig'.clojure_lsp.setup{}
+
+-- Rust
+require'lspconfig'.rust_analyzer.setup {}
+
 --Telescope
+require('telescope').setup{
+  defaults = {
+    layout_strategy = 'vertical',
+    layout_config = { height = 0.5 },
+  }
+}
 map('n', '<leader>fa', ':Telescope find_files<cr>')
 map('n', '<leader>ff', ':Telescope git_files<cr>')
 map('n', '<leader>fg', ':Telescope live_grep<cr>')
